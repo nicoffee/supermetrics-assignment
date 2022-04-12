a11413ecbd5bf5482c6c4133cf92bc8519f62847
Nikolay Fedoseev <fedoseevn@gmail.com>
2022-04-12 09:37:30 +0300

    (config) removed unused code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    counter: counterReducer,
-  },
+  reducer: {},
 });
 
 export type AppDispatch = typeof store.dispatch;
